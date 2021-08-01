590d8635cffc1ffb1c0e8258021604fbea6e28ea
robteaw <robteaw@gmail.com>
2021-08-01 10:34:35 -0500

    3. Delete Button Added

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function generateItems(items) {
     ⭐⭐⭐⭐⭐ ${item.rating}
     </div>
     <div class="product-price font-bold text-grey-700 text-lg">
-        ${item.price}
+        ${numeral(item.price).format("$0,0.00")}
     </div>
     `;
 
